f637fd34a2e51f454e3ef4e754edd87bfc17b6ca
JacobAmaury <amauryj99@gmail.com>
2026-04-18 22:33:17 +0200

    Genaration of the donjon is working

--- src/donjon.cpp
+++ src/donjon.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <typeinfo>
 using namespace std;
 
-enum type_Case {MUR, PASSAGE, TRESOR, MONSTRE, PIEGE};
+
+enum type_Case {MUR, 
+                PASSAGE, 
+                TRESOR, 
+                MONSTRE, 
+                PIEGE};
 
 class Case{
+    private:
+        vector<int> coord_case = {0,0};
     public:
         Case(){};
         virtual char afficher() = 0;
         virtual ~Case() = default;
+
+        vector<int> get_coord_case(){return coord_case;}
+        void set_coord_case(int x, int y){
+            coord_case[0] = x;
+            coord_case[1] = y;
+        }
 };
 
 class Mur : public Case{
@@ -16,6 +32,15 @@ class Mur : public Case{
         Mur():Case(){};
         char afficher() override{
             return '#';
+        }
+
+
+};
+class Passage : public Case{
+    public :
+        Passage():Case(){};
+        char afficher() override{
+            return ' ';
         }
 
 
@@ -30,6 +55,7 @@ class CaseFactory{
                 return new Mur();
                 break;
             case PASSAGE:
+                return new Passage();
                 break;
             case TRESOR:
                 break;
@@ -48,11 +74,18 @@ class Donjon{
         vector<vector<Case*>> grille;
         int l;
         int h;
-        Mur m;
+
+        int x_entry;
+        int y_entry;
+        int x_exit;
+        int y_exit;
+        vector<vector<int>> chemin;
     public:
         Donjon(int l, int h){
             this->l = l;
             this->h = h;
+            chemin.resize(h*l, vector<int>(2));
+
             grille.resize(h);
             for(int i=0; i<h; i++){
                 grille[i].resize(l);
@@ -71,6 +104,93 @@ class Donjon{
             }
             cout << endl;
         }
+
+        void set_entry(int x, int y){
+            x_entry = x;
+            y_entry = y;
+            grille[x][y] = CaseFactory().createCase(PASSAGE);
+        }
+
+        bool is_entry(int x, int y){
+            if((x == x_entry) and (y == y_entry)){return true;}
+            return false;
+        }
+
+        void recur_back_track(int x, int y, int index, bool forward){
+            if(is_entry(x,y) and !forward){return;}
+
+            // this->afficher();
+            enum direction {NORD=0,
+                            SUD,
+                            EST,
+                            OUEST};
+            
+            chemin[index][0] = x;
+            chemin[index][1] = y;
+
+            
+            int ny, nx;
+            int iy, ix;
+            bool is_ok = true;
+
+            vector<int> directions = {0, 1, 2, 3};
+
+            random_device rd; // voir https://www.geeksforgeeks.org/cpp/how-to-shuffle-a-vector-in-cpp/
+            mt19937 g(rd());
+
+            shuffle(directions.begin(), directions.end(), g);
+            
+
+            for(int dir=0; dir<4; dir++){
+                switch (directions[dir]){
+                case NORD:
+                    ny = y-2;
+                    nx = x;
+                    iy = y-1;
+                    ix = x;
+                    break;
+                case SUD:
+                    ny = y+2;
+                    nx = x;
+                    iy = y+1;
+                    ix = x;
+                    break;
+                case EST:
+                    ny = y;
+                    nx = x+2;
+                    iy = y;
+                    ix = x+1;
+                    break;
+                case OUEST:
+                    ny = y;
+                    nx = x-2;
+                    iy = y;
+                    ix = x-1;
+                    break;
+                }
+                if((ny >= h) or (ny < 0) or (nx >= l) or (nx < 0)){
+                    is_ok = false;
+                }   
+                else
+                {
+                    if(typeid(*grille[nx][ny]) == typeid(Mur)){
+                        grille[ix][iy] = CaseFactory().createCase(PASSAGE);
+                        grille[nx][ny] = CaseFactory().createCase(PASSAGE);
+                        recur_back_track(nx, ny, ++index, forward);
+                    }
+                    else{
+                        is_ok = false;
+                    }
+                }
+            }         
+            if(not(is_ok)){
+                if(index == 0){return;}
+                recur_back_track(chemin[index-1][0], chemin[index-1][1], index-1, not(forward));
+            }
+            else{
+                return;
+            }
+        }
     
 };
 
@@ -78,6 +198,9 @@ class Donjon{
 int main(){
 
     Donjon D1(15, 15);
+    D1.set_entry(0,0);
+    D1.afficher();
+    D1.recur_back_track(0,0,0,true);
     D1.afficher();
 
     return 0;
